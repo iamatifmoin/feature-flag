b2d04b7d2c24db55d922cb636cb499aab82b653e
iamatifmoin <atifmoin3@gmail.com>
2026-06-28 14:35:10 +0530

    fix: base url changed to deployment

--- user/lib/api.ts
+++ user/lib/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000'
 
 export async function apiFetch<T>(
   path: string,
